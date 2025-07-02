9485d5027de1e13efe0722d729c94cbafdb3c15a
Dama5323 <your-deenyashke@gmail.com>
2025-07-03 01:29:07 +0300

    a Bash script that creates an RSA key pair.

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Connecting without a password
+
+file_line { 'Declare identity file':
+  path  => '/root/.ssh/config',
+  line  => '    IdentityFile ~/.ssh/school',
+  match => '^ *IdentityFile',
+}
+
+file_line { 'Turn off passwd auth':
+  path  => '/root/.ssh/config',
+  line  => '    PasswordAuthentication no',
+  match => '^ *PasswordAuthentication',
+}
