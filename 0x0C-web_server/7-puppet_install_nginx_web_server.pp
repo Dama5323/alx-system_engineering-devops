500aae87a012c4829308460486164b75c5f93fe1
Dama5323 <your-deenyashke@gmail.com>
2025-07-06 13:20:10 +0300

    Configure your Nginx server to have a custom 404 page that contains the string Ceci n'est pas une page.

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,4 +1,4 @@
-# Install and configure Nginx with Hello World and /redirect_me redirection
+# Install and configure Nginx with Puppet (includes index.html, 301 redirection, and custom 404 page)
 
 package { 'nginx':
   ensure => installed,
@@ -6,7 +6,16 @@ package { 'nginx':
 
 file { '/var/www/html/index.html':
   ensure  => file,
-  content => 'Hello World!',
+  content => "Hello World!\n",
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0644',
+  require => Package['nginx'],
+}
+
+file { '/var/www/html/custom_404.html':
+  ensure  => file,
+  content => "Ceci n'est pas une page\n",
   owner   => 'www-data',
   group   => 'www-data',
   mode    => '0644',
@@ -16,24 +25,29 @@ file { '/var/www/html/index.html':
 file { '/etc/nginx/sites-available/default':
   ensure  => file,
   content => @(END),
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
+    server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
 
-    root /var/www/html;
-    index index.html index.htm;
+        root /var/www/html;
+        index index.html index.htm;
 
-    server_name _;
+        server_name _;
 
-    location / {
-        try_files \$uri \$uri/ =404;
+        location / {
+            try_files \$uri \$uri/ =404;
+        }
+
+        location /redirect_me {
+            return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+        }
+
+        error_page 404 /custom_404.html;
+        location = /custom_404.html {
+            internal;
+        }
     }
-
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
-}
-  | END
+    | END
   notify  => Service['nginx'],
   require => Package['nginx'],
 }
@@ -42,11 +56,5 @@ service { 'nginx':
   ensure     => running,
   enable     => true,
   hasrestart => true,
-}
-
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => template('nginx/default.erb'),
-  notify  => Service['nginx'],
-  require => Package['nginx'],
+  hasstatus  => true,
 }
