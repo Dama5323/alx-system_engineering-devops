823d8b9e6845040926acd8e7fb0da0e16b497c93
Dama5323 <your-deenyashke@gmail.com>
2025-07-06 12:03:34 +0300

    Configure your Nginx server to have a custom 404 page that contains the string Ceci n'est pas une page.

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,30 +1,24 @@
-# 7-puppet_install_nginx_web_server.pp
-# Configures Nginx with a Hello World homepage and a 301 redirect using Puppet
-
-package { 'nginx':
-  ensure => installed,
-}
-
-service { 'nginx':
-  ensure     => running,
-  enable     => true,
-  hasrestart => true,
-  hasstatus  => true,
-  require    => Package['nginx'],
-}
-
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => 'Hello World!',
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0644',
-  require => Package['nginx'],
-}
-
 file { '/etc/nginx/sites-available/default':
   ensure  => file,
-  content => template('nginx/default.erb'),
-  notify  => Service['nginx'],
+  content => @(END),
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+    index index.html;
+
+    server_name _;
+
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+}
+  | END
   require => Package['nginx'],
+  notify  => Service['nginx'],
 }
