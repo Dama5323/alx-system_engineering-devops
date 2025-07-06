0bb38b3c81a43164462a9eed9aa6a0ba1900b97b
Dama5323 <your-deenyashke@gmail.com>
2025-07-06 21:22:36 +0300

    Add a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,5 +1,5 @@
 # 2-puppet_custom_http_response_header.pp
-# Install Nginx and add X-Served-By header with hostname using Puppet
+# Puppet manifest to install Nginx and configure it to return a custom header with the hostname
 
 package { 'nginx':
   ensure => installed,
@@ -14,7 +14,7 @@ file { '/var/www/html/index.html':
 }
 
 exec { 'insert_custom_header':
-  command => "sed -i '/location \\/ {/a \\\\tadd_header X-Served-By \"$(hostname)\" always;' /etc/nginx/sites-available/default",
+  command => "sed -i '/location \\/ {/a \\    add_header X-Served-By \"'\\\$(hostname)'\";' /etc/nginx/sites-available/default",
   unless  => "grep 'add_header X-Served-By' /etc/nginx/sites-available/default",
   require => Package['nginx'],
 }
