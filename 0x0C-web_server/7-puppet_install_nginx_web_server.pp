473c98c8f46f22656f71d4c7626d162337962625
Dama5323 <your-deenyashke@gmail.com>
2025-07-06 11:46:15 +0300

    Install Nginx web server (w/ Puppet)

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,30 @@
+# 7-puppet_install_nginx_web_server.pp
+# Configures Nginx with a Hello World homepage and a 301 redirect using Puppet
+
+package { 'nginx':
+  ensure => installed,
+}
+
+service { 'nginx':
+  ensure     => running,
+  enable     => true,
+  hasrestart => true,
+  hasstatus  => true,
+  require    => Package['nginx'],
+}
+
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0644',
+  require => Package['nginx'],
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => template('nginx/default.erb'),
+  notify  => Service['nginx'],
+  require => Package['nginx'],
+}
